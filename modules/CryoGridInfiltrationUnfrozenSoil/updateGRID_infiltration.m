eb923d7c49fa0251af4e5738e241706808c9159e
Jan Nitzbon <jan.nitzbon@posteo.de>
2018-03-06 17:09:24 +0100

    bugfix for upper water cells also included on branch xice_mpi.

--- modules/CryoGridInfiltrationUnfrozenSoil/updateGRID_infiltration.m
+++ modules/CryoGridInfiltrationUnfrozenSoil/updateGRID_infiltration.m
@@ -29,7 +29,6 @@ function [ wc, GRID, surface_runoff ] = updateGRID_infiltration(wc, GRID, PARA, 
         GRID.soil.cT_mineral(1)=[];
         GRID.soil.cT_soilType(1)=[];
 
-
         GRID.soil.excessGroundIce(1)=[];
 
     end
@@ -63,7 +62,7 @@ function [ wc, GRID, surface_runoff ] = updateGRID_infiltration(wc, GRID, PARA, 
         % update remaining soil fields with exception of cT_water
         GRID.soil.cT_organic =  [ 0 ; GRID.soil.cT_organic ];
         GRID.soil.cT_natPor =   [ GRID.soil.cT_natPor(1); GRID.soil.cT_natPor ];    % take natPor of cell below
-        GRID.soil.cT_actPor =   [ 1; GRID.soil.cT_actPor ];                         % set to 1
+        GRID.soil.cT_actPor =   [ 1; GRID.soil.cT_actPor ];                         % set actual porosity to 1
         GRID.soil.cT_mineral =  [ 0 ; GRID.soil.cT_mineral ];
         GRID.soil.cT_soilType = [ 1; GRID.soil.cT_soilType];                        % assume sand as soil type for water cell
 
