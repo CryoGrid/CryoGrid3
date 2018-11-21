5d35769729857273552a9294a4e0f6e4e38be961
Jan Nitzbon <jan.nitzbon@posteo.de>
2018-11-21 15:39:38 +0100

    minor changes to setup and execute files.

--- execute.sh
+++ execute.sh
@@ -6,14 +6,14 @@ printf "Switched to CryoGrid3 directory...\n"
 
 # run setup script which generates setup files
 matlab -nosplash -nodisplay -nodesktop < setup_CryoGrid3.m > MATLAB.temp
-rm MATLAB.temp
+#rm MATLAB.temp
 printf "Configured run and created output directory...\n"
 
 # generate run and submit scripts from templates
 sed -e "s@SAVEDIR@$(< SAVEDIR.temp)@g" -e "s/RUN/$(< RUN.temp)/g" submit_SLURM.template > submit_SLURM.sh
 sed -e "s@SAVEDIR@$(< SAVEDIR.temp)@g" -e "s/RUN/$(< RUN.temp)/g" run_CryoGrid3.template > run_CryoGrid3.m
-rm SAVEDIR.temp
-rm RUN.temp
+#rm SAVEDIR.temp
+#rm RUN.temp
 printf "Created run and submit scripts from templates...\n"
 
 # submit the batch job to the slurm queue
--- setup_CryoGrid3.m
+++ setup_CryoGrid3.m
@@ -15,51 +15,65 @@ SETUP = {};
 SETUP.numRealizations = 3;
 SETUP.syncTimestep=6./24;
 SETUP.startDate = datenum( 1979, 10, 1 );
-SETUP.endDate = datenum( 1980, 4, 1);
-SETUP.xH=0;
-SETUP.xW=0;
+SETUP.endDate = datenum( 2099, 12, 31);
+SETUP.xH=1;
+SETUP.xW=1;
 SETUP.xS=1;
-SETUP.xice=0;
-
-SETUP.rainFrac=0.;
-SETUP.snowFrac= 2;
+SETUP.xice=1;
 
 SETUP.fieldCapacity = 0.50;
 
 
 
-SETUP.e_Reservoir = -1.5;
+SETUP.e_Reservoir = 0.0;%-1.0;
 SETUP.snowDens = 200;%200..250
 SETUP.e_R = 0.4;%0.2..0.4
 
 
-SETUP.thetaWcenterOrganicLayer = 0.85;%SETUP.fieldCapacity (VALIDATION); % set to 0.85 for saturated (LONGTERM)
+SETUP.thetaWcenterOrganicLayer = 0.85;%;SETUP.fieldCapacity; (VALIDATION); % set to 0.85 for saturated (LONGTERM)
 SETUP.f_C = 0.3;%0.3..0.5
 
 SETUP.f_T = 0.1;
 SETUP.f_R = 1.0-SETUP.f_T-SETUP.f_C;
 SETUP.e_T = SETUP.e_R-0.1;
+
 SETUP.d_xice_C=0.9;
 SETUP.d_xice_R=0.6;
 SETUP.d_xice_T1=0.2;
 SETUP.d_xice_T2=SETUP.d_xice_R-abs(SETUP.e_R-SETUP.e_T); % intermediate layer with reduced excess ice between T1 and T2, # T2 should be at the same altitude as d_xice_R
+
+d_xice_DELTA=0.0;
+SETUP.d_xice_C=SETUP.d_xice_C+d_xice_DELTA;
+SETUP.d_xice_R=SETUP.d_xice_R+d_xice_DELTA;
+SETUP.d_xice_T1=SETUP.d_xice_T1+d_xice_DELTA;
+SETUP.d_xice_T2=SETUP.d_xice_T2+d_xice_DELTA;
+
 SETUP.K_Reservoir = 5e-5;
 SETUP.K=1e-5;
 SETUP.relMaxWater = 1.;
 SETUP.heatExchangeAltitudeFactor = 0.;
 SETUP.natPor = 0.55;
 
+SETUP.scenario='rcp85';
 
-SETUP.forcingFile = 'PB_ERAinterim_1979_2017_corr.mat';
-%SETUP.forcingFile = 'samoylov_ERA_obs_fitted_1979_2014_spinup.mat';
+SETUP.forcingFile = ['Samoylov_' SETUP.scenario '_1901_2300_CryoGrid_windModified.mat'];
 
 % output directory
-SETUP.saveDir = '/data/scratch/nitzbon/CryoGrid/CryoGrid3_infiltration_xice_mpi_polygon/runs/BUGFIX/';
+SETUP.saveDir = '/data/scratch/nitzbon/CryoGrid/CryoGrid3_infiltration_xice_mpi_polygon/runs';
 
 % compose runname
-SETUP.runName = sprintf( [ 'BUGFIX_PrudhoeBayCorr_' datestr( SETUP.startDate, 'yyyymm' ) '-' datestr(SETUP.endDate, 'yyyymm' ) '_xice%d_xH%d_xW%d_xS%d_rf%0.1f_sf%0.1f' ], ...
-     SETUP.xice, SETUP.xH, SETUP.xW, SETUP.xS, SETUP.rainFrac, SETUP.snowFrac ) ;
-
+%SETUP.runName = sprintf( [ 'VALIDATION_' datestr( SETUP.startDate, 'yyyymm' ) '-' datestr(SETUP.endDate, 'yyyymm' ) '_xice%d_xH%d_xW%d_xS%d_fC%0.1f_fR%0.1f_fT%0.1f_eR%0.2f_eT%0.2f_K%0.1e_KRes%0.1e_eRes%0.2f_fc%0.2f_snowDens%d' ], ...
+ %   SETUP.xice, SETUP.xH, SETUP.xW, SETUP.xS, ...
+  %  SETUP.f_C, SETUP.f_R, SETUP.f_T, SETUP.e_R, SETUP.e_T, ...
+   % SETUP.K, SETUP.K_Reservoir, SETUP.e_Reservoir, SETUP.fieldCapacity, SETUP.snowDens) ;
+%SETUP.runName = sprintf( [ 'LONGTERM_' datestr( SETUP.startDate, 'yyyymm' ) '-' datestr(SETUP.endDate, 'yyyymm' ) '_xice%d_xH%d_xW%d_xS%d_fC%0.1f_fR%0.1f_fT%0.1f_eR%0.2f_eT%0.2f_dxiceC%0.2f_dxiceR%0.2f_dxiceTa%0.2f_dxiceTb%0.2f_K%0.1e_KRes%0.1e_eRes%0.2f_snowDens%d' ], ...
+ %    SETUP.xice, SETUP.xH, SETUP.xW, SETUP.xS, ...
+  %   SETUP.f_C, SETUP.f_R, SETUP.f_T, SETUP.e_R, SETUP.e_T, SETUP.d_xice_C, SETUP.d_xice_R, SETUP.d_xice_T1, SETUP.d_xice_T2, ...
+   %  SETUP.K, SETUP.K_Reservoir, SETUP.e_Reservoir, SETUP.snowDens) ;
+SETUP.runName = sprintf( [ 'SCENARIO' SETUP.scenario '_' datestr( SETUP.startDate, 'yyyymm' ) '-' datestr(SETUP.endDate, 'yyyymm' ) '_xice%d_xH%d_xW%d_xS%d_fC%0.1f_fR%0.1f_fT%0.1f_eR%0.2f_eT%0.2f_dxiceC%0.2f_dxiceR%0.2f_dxiceTa%0.2f_dxiceTb%0.2f_K%0.1e_KRes%0.1e_eRes%0.2f_snowDens%d' ], ...
+     SETUP.xice, SETUP.xH, SETUP.xW, SETUP.xS, ...
+     SETUP.f_C, SETUP.f_R, SETUP.f_T, SETUP.e_R, SETUP.e_T, SETUP.d_xice_C, SETUP.d_xice_R, SETUP.d_xice_T1, SETUP.d_xice_T2, ...
+     SETUP.K, SETUP.K_Reservoir, SETUP.e_Reservoir, SETUP.snowDens) ;
 [~, SETUP.git_commit_hash] = system('git rev-parse HEAD');
 
 % create ouput directory
