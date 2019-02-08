4985eac4c865a7995d063d7f26d694b05ad92c01
LeoGooga <leo.doug.martin@gmail.com>
2019-02-08 16:34:54 +0100

    Modifications to be able to load a final state from a crashed run based on Jan's scripts.

---
+++ modules/cryoGridTechnical/startFromRunSETUP.m
@@ -0,0 +1,43 @@
+function [SETUP] = startFromRunSETUP(startFromRun)
+% Function that generate a setup file to restart a run from a FINAL state
+% matfile
+
+if isempty(startFromRun)==0;
+    SETUP.flag=1;
+    
+    if ispc
+        slash='\';
+        k = strfind(startFromRun,'/');
+        startFromRun(k)=slash;
+    else
+        slash='/';
+        k = strfind(startFromRun,'\');
+        startFromRun(k)=slash;
+    end
+    
+    % load settings to have the number of workers and stuff
+    k = strfind(startFromRun,'_realization');
+    run_name=startFromRun(1:k-1);
+    k= strfind(startFromRun,'_final');
+    name_real=startFromRun(1:k-1);
+    load(['runs' slash run_name slash name_real '_settings.mat'])
+    SETUP.nbreal=length(PARA.ensemble.weight);
+    SETUP.run_name_old=run_name;
+    SETUP.run_name_new=[run_name '_restart' startFromRun(end-7:end-4)];
+    SETUP.restartyear=str2double(startFromRun(end-7:end-4));
+    assert(~isnan(SETUP.restartyear),'Error when finding the restart year')
+    SETUP.forcingname=PARA.forcing.filename;
+    SETUP.slash=slash;
+    SETUP.saveDir = ['.' slash 'runs'];
+    
+    mkdir([ SETUP.saveDir slash SETUP.run_name_new]);
+    
+else
+    
+    SETUP.flag=0;
+    SETUP.mess='no FINAL state was loaded';
+    
+end
+
+end
+
