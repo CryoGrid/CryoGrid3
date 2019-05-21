a5f546009ded9421cb600389432bbe5b12b2a92f
LeoGooga <leo.doug.martin@gmail.com>
2019-05-21 14:10:04 +0200

    Attempt with 20cm wide workers, but crashes, need to be bug fixed

--- CryoGrid3_xice_mpi_palsa_newsnow.m
+++ CryoGrid3_xice_mpi_palsa_newsnow.m
@@ -21,7 +21,7 @@ startFromRun=[]; %'190426_7w100y_roundPalsa15m_realization3_finalState1948';
 SETUP = startFromRunSETUP(startFromRun,'_v1');
 
 if SETUP.flag==0;
-    number_of_realizations=7;  % <------ Number of realization !!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+    number_of_realizations=14;  % <------ Number of realization !!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 else
     number_of_realizations=SETUP.nbreal;
 end
@@ -32,8 +32,8 @@ end
 
 % Name, Forcing and diary
 if SETUP.flag==0;
-    run_number= [datestr(date,'yymmdd') '_7w100y_redoPalsa150cm'];
-    forcingname='Suossjavri_WRF_Norstore_adapted100yr.mat';
+    run_number= [datestr(date,'yymmdd') '_14w50y_redo3mDiscret'];
+    forcingname='Suossjavri_WRF_Norstore_adapted50yr.mat';
 else
     run_number=SETUP.run_name_new;
     forcingname=SETUP.forcingname;
@@ -103,8 +103,8 @@ spmd
         %------ model parameters --------------------------------------------------
         
         % geometry configuration
-        PARA.ensemble.geomSetup=1; % Numbver of the geometrical Setup
-        PARA.technical.saving=-1; % Adjust the amount of files saved. % -1: Normal outputs, 1: light, 2: monthly means, 3: yearly means, x10(10,20,30): +Plot, x100(100,200,300): +Plot +FINAL
+        PARA.ensemble.geomSetup=5; % Numbver of the geometrical Setup
+        PARA.technical.saving=20; % Adjust the amount of files saved. % -1: Normal outputs, 1: light, 2: monthly means, 3: yearly means, x10(10,20,30): +Plot, x100(100,200,300): +Plot +FINAL
         
         % parameters related to soil
         PARA.soil.albedo=0.2;       % albedo snow-free surface
@@ -254,7 +254,7 @@ spmd
         % support shrinking when the Xice module cannot route the water up
         % because of overlying frozen cells.
         if PARA.modules.xice && PARA.modules.exchange_heat;
-            flag = excessGroundIceCheckThickness(GRID, 0.02); % Here 0.02 m is the minimum thickness that is tolerated
+            flag = excessGroundIceCheckThickness(GRID, 0.01); % Here 0.02 m is the minimum thickness that is tolerated
             assert(flag ~= 0,'Cells to small for the amount of excess ice')
         end
         
