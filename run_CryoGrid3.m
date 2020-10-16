a3242194a862a88a911458dc95383b30bcaf538f
Jan Nitzbon <jan.nitzbon@posteo.de>
2020-10-16 14:48:30 +0200

    Updated run script according to the revised setup files.

--- run_CryoGrid3.m
+++ run_CryoGrid3.m
@@ -1,15 +1,8 @@
 clear all;
 close all;
 
-%% specify setup file
-% homogeneous micro-scale representation
-setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_simple_translational_Nmeso2_eRes-0.10_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
-%setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_simple_translational_Nmeso2_eRes-10.00_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
-%setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_simple_translational_Nmeso3_eRes-10.00_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
-% polygon micro-scale representation
-%setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_polygonCRT_translational_Nmeso2_eRes0.00_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
-%setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_polygonCRT_translational_Nmeso2_eRes-10.00_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
-%setupFile = './setups/POLYGON-LANDSCAPE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_polygonCRT_translational_Nmeso3_eRes-10.00_setup.mat';        % load temporary setup file created by "setup_CryoGrid3.m"
+%% specify setup file (see subdirectory "./setups/" for further setup files
+setupFile = 'SINGLE-TILE_199910-209912_rcp85_xice1_xE1_xH1_xW1_xS1_translational_Nmeso2_xEm0_xHm0_xWm1_Dmeso100_slope0.000_eRes-10.00_setup';
 
 %% run the model
 load( setupFile );
